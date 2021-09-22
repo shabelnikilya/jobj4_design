00caaaef229866def17643d74228d1a0b3bfe733
shabelnikilya <shabelnik_ilya@mail.ru>
2021-09-22 13:45:49 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -0,0 +1,79 @@
+package ru.job4j.collection;
+
+import ru.job4j.list.List;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements List<T> {
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, container.length);
+        T rsl = container[index];
+        container[index] = newValue;
+        return rsl;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, container.length);
+        T rsl = container[index];
+        System.arraycopy(container
+                , index + 1
+                , container, index
+                , container.length - index - 1);
+        container[container.length - 1] = null;
+        size--;
+        modCount++;
+        return rsl;
+    }
+
+    @Override
+    public T get(int index) {
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            int cursor;
+            int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public T next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                } else if (cursor >= size) {
+                    throw new NoSuchElementException();
+                }
+                return container[cursor++];
+            }
+        };
+    }
+}
