fba6a7ec72efd4f4f04f72280835450ce92b52aa
shabelnikilya <shabelnik_ilya@mail.ru>
2021-09-23 15:06:52 +0300

    1. Динамический список на массиве. [#158 #206766] - изменения

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -24,7 +24,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         T rsl = container[index];
         container[index] = newValue;
         return rsl;
@@ -32,7 +32,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         T rsl = container[index];
         System.arraycopy(container
                 , index + 1
@@ -46,6 +46,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
