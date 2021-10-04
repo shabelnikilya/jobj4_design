2baad98ea5cec7173e4018b2d9b1a651d1894e6b
shabelnikilya <shabelnik_ilya@mail.ru>
2021-10-04 13:59:28 +0300

    2. Анализ доступности сервера. [#859 #211449] - изменения

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,26 +1,24 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
             boolean trouble = false;
-            List<String> s = reader.lines()
-                    .collect(Collectors.toList());
-            for (String in : s) {
-                if (("400".equals(in.split(" ")[0]) && !trouble)
-                        || ("500".equals(in.split(" ")[0]) && !trouble)) {
-                    out.print(in.split(" ")[1] + ";");
+            String line = reader.readLine();
+            while (line != null) {
+                if (("400".equals(line.split(" ")[0]) && !trouble)
+                        || ("500".equals(line.split(" ")[0]) && !trouble)) {
+                    out.print(line.split(" ")[1] + ";");
                     trouble = true;
-                } else if (trouble && !"400".equals(in.split(" ")[0])
-                        && !"500".equals(in.split(" ")[0]))  {
-                    out.println(in.split(" ")[1] + ";");
+            } else if (trouble && !"400".equals(line.split(" ")[0])
+                        && !"500".equals(line.split(" ")[0]))  {
+                    out.println(line.split(" ")[1] + ";");
                     trouble = false;
                 }
+                line = reader.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -29,14 +27,14 @@ public class Analizy {
 
     public static void main(String[] args) {
         Analizy analizy = new Analizy();
-        analizy.unavailable("D:\\file_java_io\\server_source.txt"
-                , "D:\\file_java_io\\target.txt");
-        analizy.unavailable("D:\\file_java_io\\server_source_sec.txt"
-                , "D:\\file_java_io\\target_sec.txt");
+        analizy.unavailable("./server_source.txt"
+                , "./target.txt");
+        analizy.unavailable("./server_source_sec.txt"
+                , "./target_sec.txt");
         try (BufferedReader out = new BufferedReader(
-                new FileReader("D:\\file_java_io\\target.txt"));
+                new FileReader("./target.txt"));
              BufferedReader outSecond = new BufferedReader(
-                     new FileReader("D:\\file_java_io\\target_sec.txt"))) {
+                     new FileReader("./target_sec.txt"))) {
             out.lines().forEach(System.out::println);
             System.out.println();
             outSecond.lines().forEach(System.out::println);
