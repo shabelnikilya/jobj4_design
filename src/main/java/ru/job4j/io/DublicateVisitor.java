c5e1071572d6d4f8375d356ee5ce20ca50159f8b
shabelnikilya <shabelnik_ilya@mail.ru>
2021-10-07 21:27:48 +0300

    4.2. Поиск дубликатов [#315066 #213186] - изменения

--- src/main/java/ru/job4j/io/DublicateVisitor.java
+++ src/main/java/ru/job4j/io/DublicateVisitor.java
@@ -5,16 +5,29 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class DublicateVisitor extends SimpleFileVisitor<Path> {
-    private final Set<FileProperty> findCopy = new HashSet<>();
+    private final Map<FileProperty, List<Path>> findCopy = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (!findCopy.add(new FileProperty(file.toFile().length(), file.toFile().getName()))) {
-            System.out.println("Дубликат - " + file.toAbsolutePath());
+        FileProperty key = new FileProperty(file.toFile().length()
+                , file.toFile().getName());
+        if (findCopy.containsKey(key)) {
+            findCopy.get(key).add(file);
+
+        } else {
+            List<Path> val = new ArrayList<>();
+            val.add(file);
+            findCopy.put(key, val);
+        }
+        for (FileProperty f : findCopy.keySet()) {
+            if (findCopy.get(f).size() > 1) {
+                for (Path p : findCopy.get(f)) {
+                    System.out.println("Дубликат - " + p);
+                }
+            }
         }
         return super.visitFile(file, attrs);
     }
