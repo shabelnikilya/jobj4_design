58bc65e2b9fa1cbf244c4f5ce810c867cfee641a
shabelnikilya <shabelnik_ilya@mail.ru>
2021-10-15 19:55:49 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte first = 16;
+        short second = 51;
+        int third = 28;
+        long four = 2021;
+        char five = 'a';
+        float six = 89.5f;
+        double seven = 2000000.123;
+        LOG.debug("User info first = {}, second = {}, third = {}" +
+                ", four = {}, five = {}, six = {}, seven = {}"
+                , first, second, third, four, five, six, seven);
     }
 }
