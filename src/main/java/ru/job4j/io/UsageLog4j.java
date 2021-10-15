f3b42802f25ae327cb1eb8fe7537746e388d6169
shabelnikilya <shabelnik_ilya@mail.ru>
2021-10-15 17:52:43 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,7 +7,7 @@ public class UsageLog4j {
     private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
+        LOG.trace("trace message") ;
         LOG.debug("debug message");
         LOG.info("info message");
         LOG.warn("warn message");
