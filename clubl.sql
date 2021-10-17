89edf751df859c6347a3c78e2a6c0a5140d65a41
shabelnikilya <shabelnik_ilya@mail.ru>
2021-10-17 18:55:24 +0300

    0. Что такое база данных. [#6860]

---
+++ clubl.sql
@@ -0,0 +1,11 @@
+create table club(
+	id serial primary key,
+	name varchar(255),
+	year int,
+	RedColor bool
+);
+into club(name, year, redcolor) values('Manchester United', 1914, true);
+select * from club;
+update club set year = 1920;
+delete from club;
+select * from club;
