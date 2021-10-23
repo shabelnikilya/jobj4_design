f637f3e7fa0e679e5c5a06b6019a0eb7e018c343
shabelnikilya <shabelnik_ilya@mail.ru>
2021-10-23 12:46:54 +0300

    2. Группировка и агрегатные функции [#373618]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,15 +1,20 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
-                    out.write(((i * j + " | ").getBytes()));
+                    out.print(((i * j + " | ")));
                 }
-                out.write(System.lineSeparator().getBytes());
+                out.println();
             }
         } catch (Exception e) {
             e.printStackTrace();
