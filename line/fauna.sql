2c30aa59fe8c647ee16abb27db56fa7e9d3b2d61
shabelnikilya <shabelnik_ilya@mail.ru>
2021-10-23 12:07:00 +0300

    0. Select [#6861 #219331] - изменения

--- line/fauna.sql
+++ line/fauna.sql
@@ -16,7 +16,7 @@ insert into fauna(name, avg_age, discovery_date)
 values('polorogie', 6570, date '04.04.1233');
 insert into fauna(name, avg_age, discovery_date)
 values('fish', 4300, date '04.04.245');
-select * from fauna where name = 'fish';
+select * from fauna where name like '%fish%';
 select * from fauna where avg_age > 10000 and avg_age < 21000;
 select * from fauna where discovery_date is null;
 select * from fauna where discovery_date < '01.01.1950';
