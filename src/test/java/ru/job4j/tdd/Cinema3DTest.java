1b1c9480c944848237358cde36baeee33d44e4f2
shabelnikilya <shabelnik_ilya@mail.ru>
2021-11-20 18:12:44 +0300

    2. Что такое TDD? [#4918 #228370] - изменения

--- src/test/java/ru/job4j/tdd/Cinema3DTest.java
+++ src/test/java/ru/job4j/tdd/Cinema3DTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class Cinema3DTest {
+
     @Test
     public void add() {
         Cinema cinema = new Cinema3D();
@@ -32,6 +33,34 @@ public class Cinema3DTest {
         date.set(2020, 10, 10, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         assertNull(ticket);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void buyWhenIncorrectYearDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void buyWhenIncorrectPlaceCinema() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 55, 1, date);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void buyWhenThisPlaceOccupied() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, 10, 10, 23, 00);
+        Ticket ticketOne = cinema.buy(account, 3, 1, date);
+        Ticket ticketSecond = cinema.buy(account, 3, 1, date);
     }
 
     @Test
